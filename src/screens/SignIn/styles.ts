9b455fb0e715419a1b5b43c3991eab7404b75210
mathzsl <maths.soares99@gmail.com>
2024-01-06 16:22:34 -0300

    feat: Visual structure of SignIn screen

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,11 +1,24 @@
-import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
 
+export const Container = styled.ImageBackground`
+  flex: 1;
+  padding: 52px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BRAND_LIGHT};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  text-align: center;
+`;
+
+export const Slogan = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  text-align: center;
+  margin-bottom: 32px;
+`;
