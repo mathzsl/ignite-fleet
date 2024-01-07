95a87f8ddd5fe183b8d872d15841f1dc754eed86
mathzsl <maths.soares99@gmail.com>
2024-01-06 22:08:23 -0300

    fix: adjust padding top

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -7,6 +7,8 @@ import {
   VStack,
 } from "./styles";
 
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 import { Power } from "phosphor-react-native";
 
 import { useTheme } from "styled-components/native";
@@ -16,6 +18,9 @@ import { useUser, useApp } from "@realm/react";
 export function HomeHeader() {
   const user = useUser();
   const app = useApp();
+  const insets = useSafeAreaInsets();
+
+  const paddingTop = insets.top + 32;
 
   const { COLORS } = useTheme();
 
@@ -24,7 +29,7 @@ export function HomeHeader() {
   }
 
   return (
-    <Container>
+    <Container style={{ paddingTop }}>
       <Picture
         source={{
           uri: user.profile.pictureUrl,
