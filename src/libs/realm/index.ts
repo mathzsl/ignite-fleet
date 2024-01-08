000d2a76c31ae4f410051f033cce3b7a07e6b9fc
mathzsl <maths.soares99@gmail.com>
2024-01-08 20:51:51 -0300

    fix: added realm import

--- src/libs/realm/index.ts
+++ src/libs/realm/index.ts
@@ -1,6 +1,17 @@
+import Realm from "realm";
 import { createRealmContext } from "@realm/react";
 
 import { Historic } from "./schemas/Historic";
+
+const realmAccessBehavior: Realm.OpenRealmBehaviorConfiguration = {
+  type: Realm.OpenRealmBehaviorType.OpenImmediately,
+};
+
+export const syncConfig: any = {
+  flexible: true,
+  newRealmFileBehavior: realmAccessBehavior,
+  existingRealmFileBehavior: realmAccessBehavior,
+};
 
 export const { RealmProvider, useRealm, useObject, useQuery } =
   createRealmContext({
